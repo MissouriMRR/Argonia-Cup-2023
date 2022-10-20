d6c58c8a0e35f39942d9af3a73868c3fa04ecb82
Ooglely <alennavdic@gmail.com>
2022-10-20 18:38:36 -0500

    Update extract_gps function to fit the goals of the competition

--- flight/intake_gps.py
+++ flight/intake_gps.py
@@ -1,8 +1,8 @@
 """
-Contains the intake_gps() function for extracting data out of
-the provided waypoint data JSON file for the SUAS competition.
+Contains the extract_gps() function for extracting data out of
+a provided target waypoint data JSON file for the Argonia Cup competition.
 """
-from typing import Any, NamedTuple
+from typing import Any, NamedTuple, Tuple
 import json
 import argparse
 
@@ -25,54 +25,42 @@ class Waypoint(NamedTuple):
     altitude: float
 
 
-def extract_gps(path: str) -> Waypoint:
+def extract_gps(path: str) -> Tuple[Waypoint, float]:
     """
-    Returns the waypoints, boundary points, and altitude limits from a waypoint data file.
+    Returns the target location and ground altitude from a json file specified by a parameter.
     Parameters
     ----------
     path : str
-        File path to the waypoint data JSON file.
+        File path to the target data JSON file.
     Returns
     -------
-    GPSData : TypedDict[
-        list[Waypoint[float, float, int]], list[BoundaryPoint[float, float]], list[int, int]
-        ]
-        The data in the waypoint data file
-        waypoints : list[Waypoint[float, float, float]]
-            Waypoint : Waypoint[float, float, float]
-                latitude : float
-                    The latitude of the waypoint.
-                longitude : float
-                    The longitude of the waypoint.
-                altitude : float
-                    The altitude of the waypoint.
-        boundary_points : list[BoundaryPoint[float, float]]
-            BoundaryPoint : BoundaryPoint[float, float]
-                latitude : float
-                    The latitude of the boundary point.
-                longitude : float
-                    The longitude of the boundary point.
-        altitude_limits : list[int, int]
-            altitude_min : int
-                The minimum altitude that the drone must fly at all times.
-            altitude_max : int
-                The maximum altitude that the drone must fly at all times.
+    target_data : Waypoint[float, float, float]
+        latitude : float
+            The latitude of the target.
+        longitude : float
+            The longitude of the target.
+        altitude : float
+            The altitude of the target.
+    ground_altitude: float
+        The ground altitude of the launch area in ASML (above mean sea level), as the drone
+        must navigate using AMSL instead of relative launch altitude.
     """
 
     # Load the JSON file as a Python dict to be able to easily access the data
     with open(path, encoding="UTF-8") as data_file:
         json_data: dict[str, Any] = json.load(data_file)
-    
-    target: list[float] = json_data["target"]
 
-    ground_altitude: float = json_data["ground_altitude_amsl"]
+    # Define variables for the target's location
+    latitude: float = json_data["target"]["latitude"]
+    longitude: float = json_data["target"]["longitude"]
+    altitude: float = json_data["target"]["altitude"]
 
     # Package all data into the Waypoint NamedTuple to be exported
-    target_data: Waypoint = {
-        target["latitude"],
-        target["longitude"],
-        target["altitude"]
-    }
+    target_data: Waypoint = Waypoint(latitude, longitude, altitude)
+
+    # Name a variable for ground altitude to be exported
+    ground_altitude: float = json_data["ground_altitude_amsl"]
+
     return target_data, ground_altitude
 
 
@@ -83,4 +71,9 @@ if __name__ == "__main__":
     parser.add_argument("-file")
     args: argparse.Namespace = parser.parse_args()
 
-    extract_gps(vars(args)["file"])
+    # Unpack the tuple that is returned
+    target_location: Waypoint
+    launch_altitude: float
+    target_location, launch_altitude = extract_gps(vars(args)["file"])
+    print(f"Target Location: {target_location}")
+    print(f"Altitude: {launch_altitude}")
