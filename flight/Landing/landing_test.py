45db9ee7cee534237e261f1239dbf768fa3ba4de
Ooglely <alennavdic@gmail.com>
2022-10-27 19:57:55 -0500

    Add flying to 50m above target location, fixed target data file

---
+++ flight/Landing/landing_test.py
@@ -0,0 +1,75 @@
+# Pretend Landing Spot: 37.949297, -91.784501, 0
+# Landing Spot: "longitude": 37.167814, "latitude": -97.739912
+import asyncio
+from mavsdk import System
+import sys
+sys.path.append('/home/alen/Argonia-Cup-2023/flight')
+from intake_gps import Waypoint, extract_gps
+from goto import move_to
+
+async def run() -> None:
+    """
+    Tests the goto function by moving the drone to four different waypoints.
+    """
+    drone = System()
+    await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("Drone discovered!")
+            break
+
+    print("-- Arming")
+    await drone.action.arm()
+    
+    print("Getting target location and ground altitude for landing...")
+    """ target_data: Waypoint
+    ground_altitude: float
+    target_data, ground_altitude = extract_gps('/home/alen/Argonia-Cup-2023/flight/data/target_data.json')
+    print(f"Target Location: {target_data}")
+    print(f"Altitude: {ground_altitude}")
+    target_latitude = target_data[0]
+    target_longitude = target_data[1] """
+    target_latitude = 37.949297 
+    target_longitude = -91.784501
+    
+    print("Getting current location to calculate mission points:")
+    async for position in drone.telemetry.position():
+        # Assign longitude/latitude (in degrees) and altitude (in meters) to variables
+        drone_lat: float = position.latitude_deg
+        drone_long: float = position.longitude_deg
+        drone_alt: float = position.relative_altitude_m
+        print(f"Current Location: {drone_lat}, {drone_long}, {drone_alt}")
+        break
+    
+    print("Calculating mission points:")
+    # While we could first just move to the latitude and longitude of the target and then go straight down,
+    # we can take a way more efficient path by using pythagorean theorem to find optimal points for our
+    # speed limit change and landing.
+    # We want to first calculate our altitude to get our multiplier for our similar triangles.
+    # Altitude:
+    speed_limit_alt = 400
+    speed_limit_mult = (drone_alt - speed_limit_alt) / drone_alt
+    # Latitude:
+    speed_limit_lat = target_latitude - ((drone_lat - target_latitude) * speed_limit_mult)
+    # Longitude:
+    speed_limit_lon = target_longitude - ((drone_long - target_longitude) * speed_limit_mult)
+    
+    print(f'   Speed Limit Latitude: {speed_limit_lat}')
+    print(f'   Speed Limit Longitude: {speed_limit_lon}')
+    print(f'   Speed Limit Altitude: {speed_limit_alt}')
+    
+    print("Moving to speed limit point...")
+    await move_to(drone, speed_limit_lat, speed_limit_lon, speed_limit_alt)
+    
+    print("Moving 50m above target location...")
+    await move_to(drone, target_latitude, target_longitude, 50)
+    
+    
+    
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
