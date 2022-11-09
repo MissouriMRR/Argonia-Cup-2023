78807d8bbfea1d1dcb65f18068ddd6f2374e8811
Davis <djdmvp@umsystem.edu>
2022-11-08 18:26:19 -0600

    finished documentation hopefully

--- flight/Landing/landing.py
+++ flight/Landing/landing.py
@@ -6,36 +6,50 @@ import goto
 async def manual_land(drone: System, Target_Latitude, Target_Longitude) -> None:
         """
         Function to increasingly slowly land the drone while honing in on the target
-        Parameters:
-                drone(System, Target_Latitude, Target_Longitude): our drone object
-        Return:
-            None
+        by using goto to lower the drone closer to the ground and to stay above the Target
+        Latitude and Longitude, while getting slower everytime it uses goto to get lower
+        It calls itself to update the drones location in the code, so it will know its position
+        until it gets around 6 inches off the ground then it will shut off the drone allowing it
+        to get to the ground from a safe height
+
+        
+        Parameters
+        ----------
+        drone : System
+            a drone object that has all offboard data needed for computation
+        Target_Latitude : float
+            a float containing the target latitude that needs to be reached
+        Target_Longitude : float
+            a float containing the target longitude that needs to be reached
+
+        Recursively calls itself to update its own position and when it calls itself
+        it should run the next goto to get even lower
         """
+
         # Lands the drone using the goto command to get it centered on the target while landing slowly
         logging.info("Landing the drone")
         async for position in drone.telemetry.position():
             current_altitude: float = round(position.relative_altitude_m, 3)
             print(current_altitude)
             if current_altitude > 10.0:
-                print("-- I do be working1")
                 print(current_altitude)
-                # Descends at 4 feet/s at altitudes > 10 meters down to 27 feets
-                await drone.action.set_current_speed(1.5)
+                # Descends at 4.5 feet/s at altitudes > 10 meters down to 27 feet
+                await drone.action.set_current_speed(4.5)
                 await goto.move_to(drone, Target_Latitude, Target_Longitude, 27)
                 print(current_altitude)
+                # Calls itself to update its location in the code
                 await manual_land(drone, Target_Latitude, Target_Longitude)
                 return
             elif 10.0 > current_altitude > 1:
-                print("-- I do be working2")
                 print(current_altitude)
-                # Descends at 1.5 feet/s at altitudes < 10 meters and > .3 meters down to 6 inches
+                # Descends at 1.5 feet/s at altitudes < 10 meters and > 1 meters down to 6 inches
                 await drone.action.set_current_speed(1.5)
                 await goto.move_to(drone, Target_Latitude, Target_Longitude, .5)
+                # Calls itself to update its location in the code
                 await manual_land(drone, Target_Latitude, Target_Longitude)
                 return
             else:
-                print("-- I do be working3")
-                # Sets downward velocity to 0 otherwise
+                # Sets downward velocity to 0, so the drone will stop moving
                 await drone.offboard.set_velocity_body(
                     sdk.offboard.VelocityBodyYawspeed(0.0, 0.0, 0.0, 0.0)
                 )
