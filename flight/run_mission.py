c2bf594ae2304249aa3b80ae98af4bc7f4e02170
Ooglely <alennavdic@gmail.com>
2022-11-10 17:46:30 -0600

    Split mission file into two, add variable path

---
+++ flight/run_mission.py
@@ -0,0 +1,59 @@
+import asyncio
+import sys
+
+from mavsdk import System
+
+import logging
+
+from mavsdk.mission import MissionItem, MissionPlan
+
+from intake_gps import Waypoint, extract_gps
+from goto import move_to
+from landing import manual_land
+import argparse
+
+async def run_mission(path='flight/data/target_data.json') -> None:
+    """
+    Tests the goto function by moving the drone to four different waypoints.
+    """
+    drone = System()
+    await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("Drone discovered!")
+            break
+
+    # Set an initial speed limit
+    await drone.action.set_maximum_speed(20)
+    
+    print("Getting target location and ground altitude for landing...")
+    target_data: Waypoint
+    ground_altitude: float
+    target_data, ground_altitude = extract_gps(path)
+    target_latitude = target_data[0]
+    target_longitude = target_data[1]
+    
+    await drone.mission.start_mission()
+
+    async for mission_progress in drone.mission.mission_progress():
+        print(f"Mission upload: {mission_progress.current}/{mission_progress.total}")
+        if mission_progress.current == mission_progress.total:
+            print("-- Mission Completed!")
+            break
+        await asyncio.sleep(5)
+
+    print("Starting landing process...")
+    await manual_land(drone, target_latitude, target_longitude)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    
+    # Read file to be used as the data file using the -file argument
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument("-file")
+    args: argparse.Namespace = parser.parse_args()
+    
+    loop.run_until_complete(run_mission(vars(args)["file"]))
--- flight/upload_mission.py
+++ flight/upload_mission.py
@@ -1,18 +1,18 @@
+import argparse
 import asyncio
 from mavsdk import System
-import sys
+from mavsdk.mission import MissionItem, MissionPlan
 
-sys.path.append("/home/alen/Argonia-Cup-2023/flight")
 from intake_gps import Waypoint, extract_gps
-from goto import move_to
-from landing import manual_land
-from mavsdk.mission import MissionPlan, MissionItem
-import logging
 
-
-async def run() -> None:
+async def upload_mission(path='flight/data/target_data.json') -> None:
     """
-    Tests the goto function by moving the drone to four different waypoints.
+    Connects to the drone and uploads the mission plan for landing the drone.
+    
+    Parameters
+    ----------
+    path : str
+        File path to the target data JSON file.
     """
     drone = System()
     await drone.connect(system_address="udp://:14540")
@@ -23,28 +23,14 @@ async def run() -> None:
             print("Drone discovered!")
             break
 
-    # Set an initial speed limit
-    await drone.action.set_maximum_speed(20)
-
     print("Getting target location and ground altitude for landing...")
-    """ target_data: Waypoint
+    target_data: Waypoint
     ground_altitude: float
-    target_data, ground_altitude = extract_gps('/home/alen/Argonia-Cup-2023/flight/data/target_data.json')
+    target_data, ground_altitude = extract_gps(path)
     print(f"Target Location: {target_data}")
     print(f"Altitude: {ground_altitude}")
     target_latitude = target_data[0]
-    target_longitude = target_data[1] """
-    target_latitude = 37.949297
-    target_longitude = -91.784501
-
-    print("Getting current location to calculate mission points:")
-    async for position in drone.telemetry.position():
-        # Assign longitude/latitude (in degrees) and altitude (in meters) to variables
-        drone_lat: float = position.latitude_deg
-        drone_long: float = position.longitude_deg
-        drone_alt: float = position.relative_altitude_m
-        print(f"Current Location: {drone_lat}, {drone_long}, {drone_alt}")
-        break
+    target_longitude = target_data[1]
 
     # Clear mission on drone if there is one
     await drone.mission.clear_mission()
@@ -89,17 +75,13 @@ async def run() -> None:
     print("Uploading mission...")
     await drone.mission.upload_mission(landing_mission)
 
-    async for mission_progress in drone.mission.mission_progress():
-        print(f"Mission upload: {mission_progress.current}/{mission_progress.total}")
-        if mission_progress.current == mission_progress.total:
-            print("-- Mission Completed!")
-            break
-        await asyncio.sleep(5)
-
-    print("Starting landing process...")
-    await manual_land(drone, target_latitude, target_longitude)
-
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    
+    # Read file to be used as the data file using the -file argument
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument("-file")
+    args: argparse.Namespace = parser.parse_args()
+    
+    loop.run_until_complete(upload_mission(vars(args)["file"]))
