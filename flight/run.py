3d6394aead1074d481fddd43775997d745c79e55
Davis <djdmvp@umsystem.edu>
2023-01-26 18:57:12 -0600

    necessary files for flight infastructure ?? :)

---
+++ flight/run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Main initial runnable file for the codebase"""
+
+import logging
+
+if __name__ == "__main__":
+    # Run system (multiprocessing) manager
+    try:
+        pass
+    except:  # pylint: disable=bare-except
+        logging.exception("Error in running system manager")
